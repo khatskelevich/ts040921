be145fbbb9e39dfdcd16c4a4691e10f63d3bf5d9
Igor Nepipenko <inepipenko@muellerone.com>
2021-09-04 11:47:30 +0300

    feat(no-ref): fix errors in utils

---
+++ src/utils.ts
@@ -0,0 +1,36 @@
+Object.defineProperty(window, 'MySweetApp', { value: 'v1.0.0', writable: true });
+
+function deliveryMethod() {
+	// TODO
+	return 'overnight';
+}
+
+function shipWeight() {
+	const el = document.getElementById('weight');
+	if (el === null) {
+		return 0;
+	}
+	return parseInt(el.innerHTML);
+}
+
+function sendUpdates(emailAddr: string | string[]) {
+	/**
+	 * @param {string} addr
+	 */
+	function sendEmail(addr: string) {
+		console.log(`Shipping to ${addr} via ${deliveryMethod() || 'standard'} delivery`);
+
+		if (shipWeight() > 100) {
+			console.log('WARNING: Oversize package');
+		}
+	}
+	if (Array.isArray(emailAddr)) {
+		emailAddr.forEach((val) => {
+			sendEmail(val.trim());
+		});
+	} else {
+		sendEmail(emailAddr.trim());
+	}
+}
+
+sendUpdates('inepipenko@gmail.com');
