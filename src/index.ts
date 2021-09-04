d164a223dc0293da7e09c56db770675ebd395111
A. Khatskelevich <a.khatskelevich@promo-bot.ru>
2021-09-04 15:11:02 +0500

    chore: добавлен sm, обновлен .gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,32 @@
-// import '../assets/css/style.css';
+import '../assets/css/style.css';
 
-/** let/const */
-let period = 10;
-const baseUrl = 'http://localhost:3000';
 
-/** object */
-const firstName = 'Igor'
-const account = {
-    firstName,
-    getName() {
-        return this.firstName
-    }, 
-    period,
-    baseUrl
-}
+// /** let/const */
+// let period = 10
+// const baseUrl = 'http://localhost:3000'
 
-/** spread */
-const person = {...account}
-let dates = [...[1,2,3]]
-
-/** destruct */
-const {firstName: username} = account
-const [firstDate] = dates
-
-/** template strings */
-function userMessage([start, end]:TemplateStringsArray, {firstName}: typeof account) {
-    return `${start}${firstName}${end}`
-}
-
-console.log(`gd, ${person}`);
+// /** object */
+// const firstName = 'Igor'
+// const account = {
+//     firstName,
+//     getName () {
+//         return this.firstName
+//     },
+//     period,
+//     baseUrl
+// }
+//
+// /** spread */
+// const person = {...account}
+// let dates = [...[1, 2, 3]]
+//
+// /** destruct */
+// const {firstName: username} = account
+// const [firstDate] = dates
+//
+// /** template strings */
+// function userMessage ([start, end]: TemplateStringsArray, {firstName}: typeof account) {
+//     return `${start}${firstName}${end}`
+// }
+//
+// console.log(`gd, ${person}`)
